6f25a44f8dd65246e338415542ffb1a8d47f71da
Alvaro Gonzalez S <agonzalezcl@gmail.com>
2021-08-26 04:54:05 +0000

    modificacion lab models

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -9,7 +9,22 @@ sap.ui.define([
 
 		return Controller.extend("logaliudemycli.logaliudemycli.controller.View1", {
 			onInit: function () {
+               const oJSONModel = new sap.ui.model.json.JSONModel(); 
+               const oView = this.getView();
 
-			}
+               oJSONModel.loadData("./model/SelectionScreenMenu.json");
+               oView.setModel(oJSONModel,"selectionScreen");
+            },
+            
+            onFilter: function () {
+
+            },
+
+            onClearFilter: function (){
+                const oModelSelScreen = this.getView().getModel("selectionScreen");
+                oModelSelScreen.setProperty("/ShipName", "");
+                oModelSelScreen.setProperty("/CountryKey", "");
+            }
+
 		});
 	});
